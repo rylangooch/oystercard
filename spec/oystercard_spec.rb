3b02df64922b3d2feb97923c7fc8523729d4eb90
Rylan Gooch <rylangooch@gmail.com>
2016-07-11 17:10:25 +0100

    Challenges 4 and 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,18 @@
-desribe Oystercard do
-  
+describe Oystercard do
+  subject { described_class.new }
+
+
+  describe "balance" do
+    it "is expected to return a float" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe "top_up" do
+    it "is expected to top up the oystercard by a specified amount" do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
+
 end
