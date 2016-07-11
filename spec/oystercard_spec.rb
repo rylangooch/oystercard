0b50139b84294cfdfb4c0a4a0d7a716c2377988c
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-11 17:33:56 +0100

    completed task 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  describe '#initialization' do
+    subject { Oystercard.new }
+  end
+
+  describe '#class methods' do
+    it { is_expected.to(respond_to(:balance)) }
+  end
+
+  describe '#balance' do
+    it 'Oystercard initializes with 0 balance' do
+      expect(subject.balance).to(eq(0))
+    end
+  end
 end
