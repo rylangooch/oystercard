a0749fb41308c7dd482b5ca519c6691a9de10a5e
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-12 09:51:20 +0100

    refactored oystercard_spec.rb

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,12 +6,17 @@ describe Oystercard do
   end
 
   describe '#class methods' do
-    it { is_expected.to(respond_to(:balance)) }
+    context 'these methods should exist in the class' do
+      it { is_expected.to(respond_to(:balance)) }
+    end
+
   end
 
   describe '#balance' do
-    it 'Oystercard initializes with 0 balance' do
-      expect(subject.balance).to(eq(0))
+    context '#new card initialized, balance eq 0' do
+      it 'Oystercard initializes with 0 balance' do
+        expect(subject.balance).to(eq(0))
+      end
     end
   end
 end
