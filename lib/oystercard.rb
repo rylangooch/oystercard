6b6eca707a99fc003e321403640055798bb7637c
Rylan Gooch <rylangooch@gmail.com>
2016-07-12 16:53:49 +0100

    Challenge #11 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,7 @@ class Oystercard
 MAXIMUM_BALANCE = 90
 MINIMUM_FARE = 1
 attr_reader :balance
+attr_reader :entry_station
 
   def initialize
     @balance = 0
@@ -14,9 +15,10 @@ attr_reader :balance
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(entry_station)
     fail "Please top up your oystercard" if @balance < MINIMUM_FARE
     @in_journey = true
+    @entry_station = entry_station
   end
 
   def in_journey?
@@ -26,6 +28,7 @@ attr_reader :balance
   def touch_out
     deduct(MINIMUM_FARE)
     @in_journey = false
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,11 @@ require 'oystercard'
 describe Oystercard do
   subject { described_class.new }
   let(:minimum_fare) { Oystercard::MINIMUM_FARE }
-  let(:maximum_balance) {Oystercard::MAXIMUM_BALANCE}
+  let(:maximum_balance) { Oystercard::MAXIMUM_BALANCE }
+  let(:entry_station) { double(:entry_station) }
+  let(:full_card) { described_class.new}
+  before(:example) { full_card.top_up(maximum_balance) }
+
   describe 'initialize' do
 
     it 'is initially not in a journey' do
@@ -18,61 +22,56 @@ describe Oystercard do
   end
 
   describe "top_up" do
-    context 'when card is fully topped up' do
-        before(:example) do
-          subject.top_up(maximum_balance)
-        end
-      it "will raise an error if card limit reached" do
-        expect {subject.top_up(minimum_fare)}.to raise_error "Unable to top up: £#{maximum_balance} limit exceeded"
-      end
-    end
-
     it "is expected to top up the oystercard by a specified amount" do
       expect{subject.top_up(minimum_fare)}.to change{ subject.balance }.by(minimum_fare)
+    end
+    context 'when card is fully topped up' do
+      it "will raise an error if card limit reached" do
+        expect {full_card.top_up(minimum_fare)}.to raise_error "Unable to top up: £#{maximum_balance} limit exceeded"
+      end
     end
   end
 
   describe 'in_journey?' do
     context 'when card is fully topped up' do
-      before(:example) do
-        subject.top_up(maximum_balance)
-      end
       it 'reports whether card is in journey' do
-        subject.touch_in
-        expect(subject).to be_in_journey
+        full_card.touch_in(entry_station)
+        expect(full_card).to be_in_journey
       end
     end
   end
 
   describe 'touch_out' do
+    it 'forgets entry station' do
+      full_card.touch_in(entry_station)
+      expect{full_card.touch_out}.to change{full_card.entry_station}.to be(nil)
+    end
     context 'when card is fully topped up' do
-      before(:example) do
-        subject.top_up(maximum_balance)
-      end
       it 'can change the status of in journey' do
-        subject.touch_in
-        expect{subject.touch_out}.to change{subject.in_journey?}.to be(false)
+        full_card.touch_in(entry_station)
+        expect{full_card.touch_out}.to change{full_card.in_journey?}.to be(false)
       end
       it 'reduces the balance by minimum fare' do
-        subject.touch_in
-        expect{subject.touch_out}.to change{subject.balance}.by(-minimum_fare)
+        full_card.touch_in(entry_station)
+        expect{full_card.touch_out}.to change{full_card.balance}.by(-minimum_fare)
       end
+
     end
   end
 
   describe 'touch_in' do
-    context 'when card is fully topped up' do
-      before(:example) do
-        subject.top_up(maximum_balance)
-      end
-      it 'can change the status of in journey' do
-        expect{subject.touch_in}.to change{subject.in_journey?}.to be(true)
-      end
+    it 'can change the status of in journey' do
+      expect{full_card.touch_in(entry_station)}.to change{full_card.in_journey?}.to be(true)
+    end
+    it 'allows the card to remember the entry station' do
+      full_card.touch_in(entry_station)
+      expect(full_card.entry_station).to eq(entry_station)
+    end
+    it 'raises an error when balance is below minimum fare' do
+      expect{subject.touch_in(entry_station)}.to raise_error "Please top up your oystercard"
     end
 
-    it 'raises an error when balance is below minimum fare' do
-      expect{subject.touch_in}.to raise_error "Please top up your oystercard"
-    end
+    it { is_expected.to respond_to(:touch_in).with(1).argument }
 
   end
 
