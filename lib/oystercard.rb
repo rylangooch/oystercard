c72d576458a93c851c063a18bca80866ee6271e5
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-12 14:22:16 +0100

    added deduct_fare method and test. completed pt.07

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,6 @@
 class Oystercard
+  MAX_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
@@ -6,6 +8,11 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise "Maximum balance of £#{MAX_BALANCE} exceeded" if (@balance + amount) > MAX_BALANCE
     @balance += amount
+  end
+
+  def deduct_fare(fare)
+    @balance -= fare
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,13 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
   describe '#initialization' do
-    subject { Oystercard.new }
+    subject { described_class.new }
   end
 
   describe '#class methods' do
     context 'these methods should exist in the class:' do
       it { is_expected.to(respond_to(:balance)) }
       it { is_expected.to(respond_to(:top_up).with(1).argument) }
+      it { is_expected.to(respond_to(:deduct_fare).with(1).argument) }
     end
 
   end
@@ -22,9 +23,24 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    context '#top_up can:' do
-      it 'top_up balance by 5' do
-        expect{ subject.top_up(5).to(change{ subject.balance }.by(5)) }
+    context '#top_up should:' do
+      it 'top up balance by 5' do
+        expect{ subject.top_up(5).to change{ subject.balance }.by(5) }
+      end
+
+      it 'not top up if max balance exceeded' do
+        max_balance = Oystercard::MAX_BALANCE
+        subject.top_up(max_balance)
+        expect{ subject.top_up(1) }.to(raise_error("Maximum balance of £#{max_balance} exceeded"))
+      end
+    end
+  end
+
+  describe '#deduct_fare' do
+    context '#deduct_fare should:' do
+      it 'deduct balance by 5' do
+        subject.top_up(10)
+        expect{ subject.deduct_fare(5).to change{ subject.balance }.by(-5) }
       end
     end
   end
