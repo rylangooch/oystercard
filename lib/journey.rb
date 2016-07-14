ee4a499a36f70b051c1a6fa2e67313d7099fd5b2
Rylan Gooch <rylangooch@gmail.com>
2016-07-14 13:07:24 +0100

    Challenge 14 started

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,28 @@
 class Journey
+  MIN_FARE = 1
+
+  def initialize
+    @journey = {}
+  end
+
+  def start(entry_station)
+    @journey[:entry_station] = entry_station
+  end
+
+  def finish(exit_station)
+      @journey[:exit_station] = exit_station
+  end
+
+  def journey
+    @journey
+  end
+
+  def fare
+    MIN_FARE
+  end
+
+  def complete?
+    @journey.has_key?(:entry_station) && @journey.has_key?(:exit_station)
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,25 +1,55 @@
 require 'journey'
 
 describe Journey do
-  describe '#initialization' do
-    subject { described_class.new }
-  end
+  subject(:journey) { described_class.new }
+  let(:entry_station) { double(:entry_station)}
+  let(:exit_station) { double(:exit_station)}
 
-  let(:card_with_money) { double(:card_with_money) }
-  allow(card_with_money).to receive(:touch_in).and_return(:entry_station)
+  it { is_expected.to(respond_to(:start).with(1).argument) }
 
-  it { is_expected.to(respond_to(:end_journey)) }
-
-  describe 'start_journey' do
-    let(:journey) { {entry_station: entry_station} }
-    it 'records the entry station' do
-      card_with_money.touch_in(:entry_station)
-      expect(subject.journeys).to include journey
+  describe 'start' do
+    it 'stores entry station' do
+      journey.start(entry_station)
+      expect(journey.journey).to eq({:entry_station => entry_station})
     end
   end
 
-  describe 'end_journey' do
-
+  describe 'finish' do
+    it 'stores exit station' do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      expect(journey.journey).to eq({:entry_station => entry_station, :exit_station => exit_station})
+    end
   end
 
+  describe '#fare' do
+    it 'calculates journey fare' do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      expect(journey.fare).to eq(1)
+    end
+  end
+
+  describe 'complete?' do
+      context 'journey has started and finished' do
+        it 'returns true' do
+          journey.start(entry_station)
+          journey.finish(exit_station)
+          expect(journey).to be_complete
+        end
+      end
+      context 'journey has started and not finished' do
+        it 'returns false' do
+          journey.start(entry_station)
+          expect(journey).not_to be_complete
+        end
+      end
+
+        context 'journey has not started and has finished' do
+          it 'returns false' do
+            journey.finish(exit_station)
+            expect(journey).not_to be_complete
+          end
+        end
+  end
 end
